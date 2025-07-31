08ddcc61b673f3ebac264ea40e6ee60ee4e05894
Yash <iamyashtiwari28@gmail.com>
2025-07-31 10:15:50 +0530

    added the initial server and frontend files

---
+++ backend/main.py
@@ -0,0 +1,69 @@
+import socket
+import os
+
+
+HOST = '0.0.0.0'
+PORT = 8080
+
+f = open("../frontend/index.html", "r")
+html = f.read()
+f.close()
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((HOST, PORT))
+server.listen()
+
+print(f"server is running on http://{HOST}:{PORT}")
+
+while True:
+    conn, addr = server.accept()
+    print(f"connection from {addr}")
+
+    request = conn.recv(1024).decode('utf-8')
+    print("request: \n", request)
+
+    if not request:
+        conn.close()
+        continue
+
+    request_line = request.splitlines()[0]
+    parts = request_line.split()
+
+    if len(parts) < 2:
+        conn.close()
+        continue
+    
+    method, path = parts[0], parts[1]
+
+    if method == 'GET':
+        if path == '/':
+            response = (
+                    "HTTP/1.1 200 OK\r\n"
+                    "Content-Type: text/html\r\n"
+                    f"Content-length: {len(html)}\r\n"
+                    "\r\n"
+                    + html )
+
+        elif path == '/style.css':
+            f = open("../frontend/style.css", "r")
+            css = f.read()
+            f.close()
+            response = (
+                    "HTTP/1.1 200 OK\r\n"
+                    "Content-Type: text/css\r\n"
+                    f"Content-length: {len(css)}\r\n"
+                    "\r\n"
+                    + css
+                    ).encode()
+    else:
+        body = b"<h1> 404 Not Found</h1>"
+        response = (
+                "HTTP/1.1 404 Not Found\r\n"
+                "Content-Type: text/html\r\n"
+                f"Content-Length: {len(body)}\r\n"
+                "\r\n"
+                ).encode() + body
+
+    conn.sendall(response if isinstance(response, bytes) else response.encode())
+    conn.close()
