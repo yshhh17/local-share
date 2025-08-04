87fb2e52ae9a37f886f673cad418289d655ee62e
Yash <iamyashtiwari28@gmail.com>
2025-08-04 22:41:10 +0530

    completed the ping pong functions

--- backend/discovery_handler.py
+++ backend/discovery_handler.py
@@ -9,7 +9,7 @@ TIMEOUT = 3
 
 def start_listening():
     sockt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(("0.0.0.0", DISCOVERY_PORT))
+    sockt.bind(("0.0.0.0", DISCOVERY_PORT))
 
     print(f"[DISCOVERY] listening for pings on port {DISCOVERY_PORT}...")
 
@@ -18,9 +18,9 @@ def start_listening():
             data, addr = sockt.recvfrom(BUFFER_SIZE)
             message = data.decode().strip()
 
-            if message == DISCOVERY_PING:
+            if message == DISCOVERY_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PING:
                 print(f"[DISCOVERY] ping from {addr[0]}")
-                sock.sendto(DISCOVERY_PONG.encode(), addr)
+                sockt.sendto(DISCOVERY_PONG.encode(), addr)
 
         except Exception as e:
             print(f"[DISCOVERY] Error in listener: {e}")
@@ -33,10 +33,30 @@ def send_broadcast():
     sockt.settimeout(TIMEOUT)
 
     try:
-        sock.sendto(DISCOVERY_PING.encode(), ("<broadcast>", DISCOVERY_PORT))
+        sockt.sendto(DISCOVERY_PING.encode(), ("<broadcast>", DISCOVERY_PORT))
         print("[DISCOVERY] broadcast sent, waiting for replies...")
 
         start_time = time.time()
 
+        while True:
 
+            if time.time() - start_time > TIMEOUT:
+                break
 
+            try:
+                data, addr = sockt.recvfrom(BUFFER_SIZE)
+                if data.decode().strip() == DISCOVERY_PONG:
+                    print(f"[DISCOVERY] pong received from address: {addr[0]}")
+                    if addr[0] not in discovered_ips:
+                        discovered_ips.append(addr[0])
+
+            except socket.timeout:
+                print("[DISCOVERY] search finished.")
+                break
+
+    except Exception as e:
+        print(f"[DISCOVERY] error in broadcast: {e}")
+    finally:
+        sockt.close()
+
+    return discovered_ips
